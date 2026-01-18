881621248507eb4fb6c2de05e1a3edcdf9fb3aa9
Jacek Pasierb <jack_byk@o2.pl>
2026-01-18 23:53:52 +0100

    First

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,26 @@
 import "./globals.css";
-import type { Metadata } from "next";
+import type {Metadata} from "next";
 
 export const metadata: Metadata = {
   title: "Dojazdy Bogdanka",
-  description: "Tablica ogłoszeń dojazdów do i z Bogdanki. Bez kont. Bez logowania.",
+  description:
+    "Tablica ogłoszeń dojazdów do i z Bogdanki. Bez kont. Bez logowania.",
+  icons: {
+    icon: [
+      {url: "/favicon-16x16.png", sizes: "16x16", type: "image/png"},
+      {url: "/favicon-32x32.png", sizes: "32x32", type: "image/png"},
+    ],
+    apple: "/apple-touch-icon.png",
+    other: [
+      {
+        rel: "manifest",
+        url: "/site.webmanifest",
+      },
+    ],
+  },
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
     <html lang="pl">
       <body>{children}</body>
