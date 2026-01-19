bc51bf39af8ae2fc3c18eebde2bd77bcc212ebf2
Jacek Pasierb <jack_byk@o2.pl>
2026-01-19 13:49:27 +0100

    First

--- src/components/rides/Footer.tsx
+++ src/components/rides/Footer.tsx
@@ -16,12 +16,12 @@ export default function Footer() {
       <p className="mt-3">
         Masz uwagi, pomysł na ulepszenie lub chcesz pomóc w rozwoju?
         <br />
-        Napisz do nas:
+        Napisz do nas: 
         <a
           href="mailto:dojazdy.bogdanka@o2.pl"
           className="font-semibold text-slate-300 underline underline-offset-2 hover:text-slate-100"
         >
-          dojazdy.bogdanka@o2.pl
+           dojazdy.bogdanka@o2.pl
         </a>
       </p>
     </footer>
